f04b700cfaeb855ed7157abef57a70dfdd14cb3f
Tristan ROTH <tristan.roth3@etu.univ-lorraine.fr>
2024-03-03 17:26:34 +0100

    ajout des liens sur les icons et boutons en partie + trad

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+/*document.addEventListener("DOMContentLoaded", function() {
+    // Sélectionnez le lien de la navbar
+    var lienNavbar = document.querySelector('.navbar-brand[href="#haut-de-page"]');
+
+    // Ajoutez un écouteur d'événements pour détecter le clic sur le lien de la navbar
+    lienNavbar.addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+
+        // Faites défiler la page vers le haut
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Défilement fluide
+        });
+    });
+});
+*/
